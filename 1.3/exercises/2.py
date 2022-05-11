9ce3042e343e6365ed0bcf884ca3c7d349789017
Matt Drake <drake.mattc@gmail.com>
2022-05-10 20:24:39 -0700

    finish 2

---
+++ 1.3/exercises/2.py
@@ -0,0 +1,29 @@
+"""
+Trace:
+it
+it was
+it
+it the best
+it the
+it the of times
+it the of
+it the
+it
+it it
+it it was
+it it
+it it the
+it it
+it
+===
+
+stdout:
+was
+best
+times
+of
+the
+was
+the
+it
+"""
