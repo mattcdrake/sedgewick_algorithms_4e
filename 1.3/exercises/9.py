735a84d8e6152d36a254e562bc4c6cfb6c367f86
Matt Drake <drake.mattc@gmail.com>
2022-05-15 19:31:44 -0700

    finish 9

---
+++ 1.3/exercises/9.py
@@ -0,0 +1,27 @@
+def show_parens(expr: str):
+    """Assuming string input is valid."""
+    operands = []
+    operators = []
+
+    pieces = expr.split(" ")
+    for piece in pieces:
+        if piece == ")":
+            right = operands.pop()
+            left = operands.pop()
+            op = operators.pop()
+            operands.append(f"( {left} {op} {right} )")
+            continue
+
+        if piece.isnumeric():
+            operands.append(piece)
+        else:
+            operators.append(piece)
+
+    return operands[0]
+
+
+cases = [
+    "1 + 2 ) * 3 - 4 ) * 5 - 6 ) ) )",
+]
+for case in cases:
+    print(show_parens(case))
