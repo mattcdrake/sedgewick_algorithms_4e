dd44008526b7a764126595f266e235d1021ae44b
Matt Drake <drake.mattc@gmail.com>
2022-05-15 19:40:44 -0700

    finish 10

--- 1.3/exercises/10.py
+++ 1.3/exercises/10.py
@@ -1,15 +1,18 @@
-def include_parens(expr: str) -> str:
+def infix_to_postfix(expr: str) -> str:
     """Assuming string input is valid."""
     operands = []
     operators = []
 
     pieces = expr.split(" ")
     for piece in pieces:
+        if piece == "(":
+            continue
+
         if piece == ")":
             right = operands.pop()
             left = operands.pop()
             op = operators.pop()
-            operands.append(f"( {left} {op} {right} )")
+            operands.append(f"{left} {right} {op}")
             continue
 
         if piece.isnumeric():
@@ -20,9 +23,7 @@ def include_parens(expr: str) -> str:
     return operands[0]
 
 
-cases = [
-    "1 + 2 ) * 3 - 4 ) * 5 - 6 ) ) )",
-]
+cases = ["( ( 1 + 2 ) * ( ( 3 - 4 ) * ( 5 - 6 ) ) )"]
 
 for case in cases:
-    print(include_parens(case))
+    print(infix_to_postfix(case))
