28964f0eba48a7b3c83bfa70c99045be2c7306a9
Matt Drake <drake.mattc@gmail.com>
2022-05-15 18:51:55 -0700

    Finish 1.3.6

---
+++ 1.3/exercises/6.py
@@ -0,0 +1,26 @@
+"""
+(most recent additions are at the end)
+Queue: [1, 2, 3]
+Stack: []
+
+Q: [2, 3]
+S: [1]
+
+Q: [3]
+S: [1, 2]
+
+Q: []
+S: [1, 2, 3]
+
+Q: [3]
+S: [1, 2]
+
+
+Q: [3, 2]
+S: [1]
+
+Q: [3, 2, 1]
+S: []
+
+Ans: This code fragment reverse the order of elements in a queue.
+"""
