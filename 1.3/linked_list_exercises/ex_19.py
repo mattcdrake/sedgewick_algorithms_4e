9589f4bbe5205e28a012c21a6f806ffa0d60043d
Matt Drake <drake.mattc@gmail.com>
2022-05-18 19:57:09 -0700

    Fix 19

--- 1.3/linked_list_exercises/ex_19.py
+++ 1.3/linked_list_exercises/ex_19.py
@@ -1,11 +1,17 @@
 """
 ...create ll...
 
-cur = ll.first
-if cur.next is None:
+# Empty list
+if ll.first is None:
+    return
+
+# One item
+if ll.first.next is None:
     ll.first = None
     return
 
+# General case
+cur = ll.first
 while cur.next is not None:
     prev = cur
     cur = cur.next
